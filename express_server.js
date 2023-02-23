033f2e848e3fc1d067a165b865f5dee15f43b56c
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-23 09:15:35 -0500

    created login form and stored username with cookies

--- express_server.js
+++ express_server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const cookieParser = require('cookie-parser');
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 function generateRandomString() {
   const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
@@ -19,29 +21,25 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
-
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase, 
+    username: req.cookies["username"] };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  const templateVars = { 
+    username: req.cookies["username"]
+};
+  res.render("urls_new", templateVars);
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = { 
+    id: req.params.id, 
+    longURL: urlDatabase[req.params.id], 
+    username: req.cookies["username"] };
   res.render("urls_show", templateVars);
 });
 
@@ -69,6 +67,12 @@ app.get("/u/:id", (req, res) => {
 app.post('/urls/:id', (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL
   res.redirect("/urls");
+})
+
+app.post('/login', (req, res) => {
+  res.cookie("username",req.body.username);
+  console.log(req.body.username);
+  res.redirect('/urls');
 })
 
 
