0cb7d88e5a6c5eac90df232d298873835b406d06
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-23 11:57:35 -0500

    added log out button and logout post route to clear cookies and redirect to same page

--- express_server.js
+++ express_server.js
@@ -6,6 +6,7 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Generating random string for shortURL
 function generateRandomString() {
   const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
   let result = '';
@@ -43,35 +44,49 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+// Deleting URL from URLS page and staying on current page after
 app.post("/urls/:id/delete", (req, res) => {
   delete urlDatabase[req.params.id]
   res.redirect("/urls")
   });
 
+
+  // After clciking edit button redirecting to corresponding page to change URL
   app.post("/urls/:id/edit", (req, res) => {
     res.redirect(`/urls/${req.params.id}`);
     });
 
-app.post("/urls/", (req, res) => {
+// Adding a new URL
+app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
   const urlId = generateRandomString();
   urlDatabase[urlId] = req.body.longURL
   res.redirect("/urls");
 });
 
+// Redirect shortURL to longURL website
 app.get("/u/:id", (req, res) => {
-  const longURL = urlDatabase[req.params.id]
+  const longURL = urlDatabase[req.params.id];
   res.redirect(longURL);
 });
 
+
+// Recieving information about updated LongURL 
 app.post('/urls/:id', (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL
   res.redirect("/urls");
 })
 
+// Login and store username in cookie and redirect to URLs page
 app.post('/login', (req, res) => {
   res.cookie("username",req.body.username);
   console.log(req.body.username);
+  res.redirect('/urls');
+})
+
+// Logout
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
   res.redirect('/urls');
 })
 
