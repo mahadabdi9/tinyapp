80b76cbbedcae72c4cfc832501dc618023f713cd
mahadabdi9 <mahadabdi9@gmail.com>
2023-02-27 13:31:37 -0500

    added login page and updated and register page and updated express.server with new functionality

--- express_server.js
+++ express_server.js
@@ -17,21 +17,41 @@ function generateRandomString() {
 }
 
 
+const authenticateUser = function(database, email, password) {
+  if (password === undefined) {
+    for (let user in database) {
+      if (database[user].email === email) {
+        return user;
+      }
+    }
+  } else {
+    for (let user in database) {
+      if (database[user].email === email && database[user].password === password) {
+        return user;
+      }
+    }
+  } return false;
+};
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+const users = {
+};
+
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase, 
-    username: req.cookies["username"] };
+    username: users[req.cookies["user_id"]]};
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"]
+    username: users[req.cookies["user_id"]]
 };
   res.render("urls_new", templateVars);
 });
@@ -40,7 +60,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[req.params.id], 
-    username: req.cookies["username"] };
+    username: users[req.cookies["user_id"]]};
   res.render("urls_show", templateVars);
 });
 
@@ -77,6 +97,7 @@ app.post('/urls/:id', (req, res) => {
   res.redirect("/urls");
 })
 
+/*
 // Login and store username in cookie and redirect to URLs page
 app.post('/login', (req, res) => {
   res.cookie("username",req.body.username);
@@ -84,18 +105,69 @@ app.post('/login', (req, res) => {
   res.redirect('/urls');
 })
 
-// Logout
-app.post('/logout', (req, res) => {
-  res.clearCookie('username');
-  res.redirect('/urls');
-})
+*/
 
-// get for registration page
-app.get("/user_registration", (req, res) => {
-  res.render("user_registration");
+app.post('/login', (req, res) => {
+  if (authenticateUser(users, req.body.email) === false) {
+    res.status(403);
+    res.send("Email not found, please register");
+    return;
+  } else if (authenticateUser(users, req.body.email, req.body.password) === false) {
+    res.status(403);
+    res.send("Incorrect password, please try again");
+    return;
+  } else {
+    res.cookie('user_id', authenticateUser(users, req.body.email, req.body.password));
+    res.redirect('/urls');
+  }
 });
 
+app.get("/login", (req, res) => {
+  const templateVars = { username: users[req.cookies["user_id"]] };
+  res.render("login", templateVars);
+})
 
+// Logout
+app.post('/logout', (req, res) => {
+  res.clearCookie('user_id');
+  res.redirect('/urls');
+});
+
+// get for request page
+app.get("/register", (req, res) => {
+  const userID = req.cookies["user_id"];
+  const templateVars = { username: users[req.cookies["user_id"]] };
+  console.log("This is a cookie", req.cookies);
+  setTimeout(function() { console.log("this is username ", users[userID]) }, 1000);
+  res.render("register", templateVars);
+});
+
+// registration handler
+app.post('/register', (req, res) => {
+  // error handling
+  if (!req.body.email || !req.body.password) {
+    res.status(400);
+    res.send("Please enter a valid email & password");
+    return;
+  }
+  if (authenticateUser(users, req.body.email) !== false) {
+    res.status(400);
+    res.send("Email already exists, please log in");
+    return;
+  }
+  // initialize user objs
+  const userID = generateRandomString();
+  users[userID] = {
+    id: userID,
+    email: req.body.email,
+    password: req.body.password
+  }
+
+  console.log(users);
+  //cookie time
+  res.cookie('user_id', userID);
+  res.redirect('/urls');
+})
 
 
 
